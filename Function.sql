b12bc8d9c4bea50e8a027e5888fe18d3e95878fb
sallystang01 <hunterduerr@gmail.com>
2018-01-09 19:58:09 -0500

    Updated but sadly it does not work..

--- Function.sql
+++ Function.sql
@@ -1,21 +1,53 @@
 
 
-
-ALTER FUNCTION dbo.fnInventory
+GO
+ALTER FUNCTION dbo.fnInventoryTest
 (
 @product INT
 ) 
-RETURNS int   
+RETURNS varchar(200) 
 AS   
 
-BEGIN  
-    DECLARE @avail int;  
-    SELECT  i.available
-    FROM inventory i   
-    WHERE i.available = @Avail  
-     IF (@avail = 0)   
-        SET @avail = 0;
-	ELSE
-		SET @avail = 1;  
-    RETURN @avail;  
+BEGIN 
+ 
+	DECLARE @Out VARCHAR(10)
+	
+
+	SET @Out = ( select i.inventory_id, i.location
+						from inventory i
+						where i.location = 'Out'
+	)
+RETURN 
+	CASE
+		WHEN @Product = @Out THEN 'Checked Out'
+		Else 'In Stock'
+
+	END
+ 
+    
+
 END; 
+--GO
+--CREATE FUNCTION fnLongDateTest
+--(
+--	@FullDate DATETIME
+--)
+--RETURNS VARCHAR(MAX)
+
+--	AS
+--	BEGIN
+
+--		RETURN 
+--		DATENAME(DW, @FULLDATE) + ' ' +
+--		DATENAME(D, @FULLDATE) + ' ' +
+--		DATENAME(M, @FULLDATE) + ' ' +
+--		DATENAME(YY, @FULLDATE)
+		
+
+
+--END
+
+--select dbo.fnLongDateTest(indate)
+--from inventory
+
+select dbo.fnInventoryTest(2001)
