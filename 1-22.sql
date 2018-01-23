5ab8fb8b76bc76239c853e798d591d7df4d1dbde
sallystang01 <hunterduerr@gmail.com>
2018-01-22 21:44:56 -0500

    Done for tonight

--- 1-22.sql
+++ 1-22.sql
@@ -28,27 +28,94 @@ SELECT * from	[Production].[ProductCharacter]('Ad')
 
 GO
 
-CREATE FUNCTION [Sales].[LatestOrders]
+ALTER FUNCTION [Sales].[LatestOrders]
 (
-	@Characters varchar(max)
+	@RecordLimit int
 
 )
-
-RETURNS @t TABLE
-
-(
 	
-	ProductName varchar(max)
+
+
+RETURNS TABLE
+AS 
+RETURN
+(
+	SELECT TOP (@RecordLimit) * FROM sales.SalesOrderHeader
+	order by OrderDate desc
+	)
+	GO
+
+SELECT * from	Sales.LatestOrders(20)
+
+--#3
+
+GO
+
+ALTER FUNCTION [Person].[PhoneNumberLookup]
+(
+	@Number varchar(max)
+
+)
+	
+
+
+RETURNS TABLE
+AS 
+RETURN
+(
+	select p.*, pp.PhoneNumber
+	from Person.Person p
+	inner join
+	Person.PersonPhone pp
+	on p.BusinessEntityID = pp.BusinessEntityID
+	where pp.PhoneNumber like '%' + @Number + '%'
+	)
+	GO
+
+	select * from Person.PhoneNumberLookup(612)
+
+--#4
+
+GO
+CREATE FUNCTION PrimeNumbers
+
+(
+@Number int
 )
 
-AS 
-BEGIN
+DECLARE @PRIME_BASE INT = 6    -- Start testing on either side of 6
+DECLARE @TEST_VALUE INT = 2
+DECLARE @UPPER_LIMIT INT = 1000000
+DECLARE @PRIME BIT = 1                -- Start by assuming the number is prime.
+DECLARE @PRIME_CANDIDATE INT = @PRIME_BASE - 1
 
-	INSERT INTO @t
-	SELECT Name
-	FROM Production.Product
-	where Name like @Characters
+WHILE @PRIME_CANDIDATE <= @UPPER_LIMIT
+    BEGIN
 
-END
 
-select * from sales.SalesOrderHeader
+    WHILE @TEST_VALUE !> SQRT(@PRIME_CANDIDATE)
+        BEGIN
+
+        IF (@PRIME_CANDIDATE % @TEST_VALUE = 0)
+            BEGIN
+                SET @PRIME = 0 -- Set prime to false.
+                BREAK
+            END
+        SET @TEST_VALUE = @TEST_VALUE + 1
+        END
+
+    IF @PRIME != 0
+        PRINT @PRIME_CANDIDATE
+
+    IF @PRIME_CANDIDATE = (@PRIME_BASE - 1)
+            SET @PRIME_CANDIDATE = @PRIME_CANDIDATE + 2
+    ELSE
+        BEGIN
+            SET @PRIME_BASE = @PRIME_BASE + 6
+            SET @PRIME_CANDIDATE = @PRIME_BASE - 1
+        END
+
+    SET @PRIME = 1 -- Reset
+    SET @TEST_VALUE = 2 -- Reset
+
+    END 
